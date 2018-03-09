45a462616503cb201f8fee2ae64b398113143526
XFWY iMac <lh99998888@163.com>
2018-03-09 10:26:07 +0800

    创建工程兵添加TodayExtension target

---
+++ TodayExtensionDemo/TodayExtension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayExtension
+//
+//  Created by apple on 2018/3/9.
+//  Copyright © 2018年 wyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ TodayExtensionDemo/TodayExtensionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodayExtensionDemo
+//
+//  Created by apple on 2018/3/9.
+//  Copyright © 2018年 wyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
